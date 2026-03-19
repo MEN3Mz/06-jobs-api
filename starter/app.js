8e60150ce7b87f15909edee69b118152ae138359
MEN3Mz <abdelmoneemelbaz@gmail.com>
2026-03-19 14:27:12 +0200

    add swagger to view api documentation

--- starter/app.js
+++ starter/app.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 //db
 
 const connectDB = require('./db/connect');
@@ -35,8 +39,13 @@ app.use(xss())
 app.use(cors())
 
 // routes
+app.get('/', (req, res) => {
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+})
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
